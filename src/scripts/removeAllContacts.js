07071ac5b5dc3fbe44efc63d40ca943db85f89d5
DmitriyBabij <132613323+DmitriyBabij@users.noreply.github.com>
2025-05-07 16:37:12 +0300

    hw1

---
+++ src/scripts/removeAllContacts.js
@@ -0,0 +1,13 @@
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+  try {
+    await writeContacts([]);
+    console.log('Усі контакти успішно видалено.');
+  } catch (error) {
+    console.error('Помилка при видаленні всіх контактів:', error.message);
+  }
+};
+
+removeAllContacts();
+
